8ddd1daa029f99cf63085ba790cd13803c5610f2
NicoFJCruz <nicofjcruz@gmail.com>
2023-05-10 13:11:27 -0300

    bugfix test

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -17,29 +17,4 @@ class PasswordConfirmationTest extends TestCase
 
         $user->delete();
     }
-
-    /* public function test_password_can_be_confirmed(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)->post('/confirm-password', [
-            'password' => 'password',
-        ]);
-        $response->assertRedirect();
-        $response->assertSessionHasNoErrors();
-
-        $user->delete();
-    }
-
-    public function test_password_is_not_confirmed_with_invalid_password(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)->post('/confirm-password', [
-            'password' => 'wrong-password',
-        ]);
-        $response->assertSessionHasErrors();
-        
-        $user->delete();
-    } */
 }
