865da8eaba0e9e7fffeb4ccbd0d866c96dbb1635
NicoFJCruz <nicofjcruz@gmail.com>
2023-05-15 18:31:56 -0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::post('/promotions/{id}', [CodePromotionController::class, 'generateCode']
 
 Route::get('/codepromotions/{id}',  [CodePromotionController::class, 'findCode']);
 
-Route::put('/codepromotions/{id}',  [CodePromotionController::class, 'findCode']);
+Route::put('/codepromotions/{id}',  [CodePromotionController::class, 'activateCode']);
 
 Route::get('/promotions', function (Request $request) {
     $promotions = Promotion::all();
