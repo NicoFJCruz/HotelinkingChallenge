1b8bf6f571bd03262e94a2035f8a4097af312d24
NicoFJCruz <nicofjcruz@gmail.com>
2023-05-15 15:47:53 -0300

    Login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import Checkbox from "@/Components/Checkbox";
+//import Checkbox from "@/Components/Checkbox";
 import GuestLayout from "@/Layouts/GuestLayout";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
@@ -70,36 +70,13 @@ export default function Login({ status, canResetPassword }) {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData("remember", e.target.checked)
-                            }
-                        />
-                        <span className="ml-2 text-sm text-gray-600">
-                            Recuérdame
-                        </span>
-                    </label>
-                </div>
-                <Link
-                    href={route("register")}
-                    className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    ¿No tienes cuenta?
-                </Link>
-
                 <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            ¿Olvidaste tu contraseña?
-                        </Link>
-                    )}
+                    <Link
+                        href={route("register")}
+                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        ¿No tienes cuenta?
+                    </Link>
 
                     <PrimaryButton className="ml-4" disabled={processing}>
                         Iniciar sesión
